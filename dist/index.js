5e47bad9a623f31059565c5e3e1495281f70f28e
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-21 20:09:20 +0100

    working on the geocode api

--- dist/index.js
+++ dist/index.js
@@ -697,7 +697,7 @@ const adminDisplayUserPackages = (id) => {
     `;
     });
   } else {
-    packagesDiv = packages;
+    packagesDiv = packages.packages;
   }
 
   newPackages.innerHTML = packagesDiv;
