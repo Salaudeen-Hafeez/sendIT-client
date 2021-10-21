e8be27e68e659a9e4d668d64b730689093b0df2d
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-21 10:31:47 +0100

    working on the geocode api

--- dist/index.js
+++ dist/index.js
@@ -681,7 +681,7 @@ const adminDisplayUserPackages = (id) => {
   const packages = JSON.parse(localStorage.getItem('userPackages'));
   const newPackages = document.getElementsByClassName(userCont);
   console.log(newPackages);
-  newPackages.classList.toggle('open');
+  // newPackages.classList.toggle('open');
   packages.forEach((packag) => {
     packagesDiv += `<li>
         <div class="userDetails">
