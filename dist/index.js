482ad93fae66a892bedef7caacbcf658a3c7277b
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-24 03:49:49 +0100

    working on the geocode api

--- dist/index.js
+++ dist/index.js
@@ -621,7 +621,8 @@ const adminDeleteUser = (e) => {
   const admin = adminsData();
   const { _email, admin_token } = admin;
   console.log(e);
-  const [username, id] = e.value;
+  const username = e.value;
+  const username = e.id;
   console.log(username, id);
   fetch(
     `https://sendit-logistic-2021.herokuapp.com/api/v1/users/${username}/${id}/${_email}/${admin_token}`,
