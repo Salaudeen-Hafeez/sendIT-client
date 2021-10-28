0256060f02294b4954405c7f2fbddc520132b5aa
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-28 12:40:58 +0100

    working on the delete package

--- dist/index.js
+++ dist/index.js
@@ -252,6 +252,7 @@ const fetchPendingPackages = () => {
   )
     .then((resp) => resp.json())
     .then((data) => {
+      console.log(data);
       localStorage.removeItem('packages');
       localStorage.setItem('packages', JSON.stringify(data));
     })
