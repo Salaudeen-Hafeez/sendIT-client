8c36b91f6b1833587255bf98e63f8f1cfe997ce2
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-28 15:17:53 +0100

    working on the delete package

--- dist/index.js
+++ dist/index.js
@@ -42,6 +42,7 @@ const formValidation = (input) => {
       data[inp.getAttribute('name')] = inp.value.trim().toLowerCase();
     } else if (inp.getAttribute('name') === 'password') {
       if (inp.value.trim().length < 6) {
+        console.log(inp.value);
         emptyInput = 'true';
         setErrorFor(inp, 'The password length must be 6 or more');
       }
