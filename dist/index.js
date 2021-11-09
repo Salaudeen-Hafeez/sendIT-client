bb5b8c1db241aca6f2afc66ec6ce745d50c689bf
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-11-09 17:37:10 +0100

    Working on the login

--- dist/index.js
+++ dist/index.js
@@ -536,9 +536,9 @@ const newPackage = () => {
 };
 
 const postPackage = (data) => {
-  console.log(data);
   const user = usersData();
   const { _username, _email, auth_token } = user;
+  data[username] = _username;
   fetch(
     `https://akera-logistics.herokuapp.com/api/v1/users/${_username}/${_email}/${auth_token}/packages`,
     {
