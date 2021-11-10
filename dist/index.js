f7f9260af8689edcdeac841b1d8cb02fa97cea13
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-11-10 09:32:41 +0100

    Working on the login

--- dist/index.js
+++ dist/index.js
@@ -956,3 +956,19 @@ function initMap() {
     geocodeAddress(parameters, address);
   });
 }
+
+var searchInput = 'location';
+
+$(document).ready(function () {
+  var autocomplete;
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById(searchInput),
+    {
+      types: ['geocode'],
+    }
+  );
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    var near_place = autocomplete.getPlace();
+  });
+});
