67a34ddbd488999816cfc49ff061d58489563590
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-11-10 12:51:39 +0100

    Working on the login

--- dist/index.js
+++ dist/index.js
@@ -164,7 +164,11 @@ const createAdmin = () => {
 
 var x = document.referrer;
 const user = usersData();
-if (!user && x !== 'https://akera-logistics.netlify.app') {
+if (
+  !user &&
+  x !== 'https://akera-logistics.netlify.app' &&
+  x !== 'https://akera-logistics.netlify.app/login'
+) {
   window.location.href = 'https://akera-logistics.netlify.app/login';
 }
 console.log(x);
