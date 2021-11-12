1c0c23c5706e610e9513611584d60aeb72079433
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-11-12 10:41:11 +0100

    Working on the login

--- dist/index.js
+++ dist/index.js
@@ -959,9 +959,6 @@ function initMap() {
   addresses.forEach((address) => {
     geocodeAddress(parameters, address);
   });
-
-  var input3 = document.getElementById('newDestination');
-  var autocomplete3 = new google.maps.places.Autocomplete(input3);
 }
 
 function autoCompleteAddress() {
@@ -969,6 +966,8 @@ function autoCompleteAddress() {
   var autocomplete = new google.maps.places.Autocomplete(input);
   var input2 = document.getElementById('destination');
   var autocomplete2 = new google.maps.places.Autocomplete(input2);
+  var input3 = document.getElementById('newDestination');
+  var autocomplete3 = new google.maps.places.Autocomplete(input3);
   // autocomplete.addListener('place_changed', function () {
   //   var place = autocomplete.getPlace();
   //   document.getElementById('address').value = JSON.stringify(
