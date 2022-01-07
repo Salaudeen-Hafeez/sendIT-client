252e915b194e71610f6279145a315ff9cecf38df
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-07 09:53:41 +0100

    working on route redirection

--- dist/index.js
+++ dist/index.js
@@ -199,15 +199,28 @@ const authenticateRoute = (pathName) => {
       }
 
       break;
-    case 'https://akera-logistics.netlify.app/user' ||
-      'https://akera-logistics.netlify.app/admin' ||
-      'https://akera-logistics.netlify.app/package':
+    case 'https://akera-logistics.netlify.app/user':
       if (authData !== null && authData.user) {
         console.log(authData);
         window.location.replace('https://akera-logistics.netlify.app/user');
-      } else if (authData !== null && authData.admin) {
+      } else {
+        console.log(authData);
+        window.location.replace('https://akera-logistics.netlify.app/login');
+      }
+      break;
+    case 'https://akera-logistics.netlify.app/admin':
+      if (authData !== null && authData.admin) {
         console.log(authData);
         window.location.replace('https://akera-logistics.netlify.app/admin');
+      } else {
+        console.log(authData);
+        window.location.replace('https://akera-logistics.netlify.app/login');
+      }
+      break;
+    case 'https://akera-logistics.netlify.app/package':
+      if (authData !== null && authData.user) {
+        console.log(authData);
+        window.location.replace('https://akera-logistics.netlify.app/package');
       } else {
         console.log(authData);
         window.location.replace('https://akera-logistics.netlify.app/login');
