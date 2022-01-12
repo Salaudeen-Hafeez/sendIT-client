255715c6d2b477ecd00ca6547344f070db75dd43
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-13 00:54:36 +0100

    Implementing modules in the project

--- dist/jsmodules/index.js
+++ dist/jsmodules/index.js
@@ -889,70 +889,70 @@ const updatePackage = () => {
   }
 };
 
-// Google map API call and services
-function geocodeAddress(parameters, address) {
-  const { geocoder, map, add, service } = parameters;
-  geocoder
-    .geocode({ address })
-    .then(({ results }) => {
-      add.push(results[0].formatted_address);
-      if (add.length >> 1) {
-        const request = {
-          origins: [add[0]],
-          destinations: [add[1]],
-          travelMode: google.maps.TravelMode.DRIVING,
-          unitSystem: google.maps.UnitSystem.METRIC,
-          avoidHighways: false,
-          avoidTolls: false,
-        };
-        // get distance matrix response
-        service.getDistanceMatrix(request).then((response) => {
-          localStorage.setItem('distanceMetrix', JSON.stringify(response));
-        });
-      }
-      map.setCenter(results[0].geometry.location);
-      new google.maps.Marker({
-        map,
-        position: results[0].geometry.location,
-      });
-    })
-    .catch((e) =>
-      alert(`Geocode was not successful for the following reason: ${e}`)
-    );
-}
+// // Google map API call and services
+// function geocodeAddress(parameters, address) {
+//   const { geocoder, map, add, service } = parameters;
+//   geocoder
+//     .geocode({ address })
+//     .then(({ results }) => {
+//       add.push(results[0].formatted_address);
+//       if (add.length >> 1) {
+//         const request = {
+//           origins: [add[0]],
+//           destinations: [add[1]],
+//           travelMode: google.maps.TravelMode.DRIVING,
+//           unitSystem: google.maps.UnitSystem.METRIC,
+//           avoidHighways: false,
+//           avoidTolls: false,
+//         };
+//         // get distance matrix response
+//         service.getDistanceMatrix(request).then((response) => {
+//           localStorage.setItem('distanceMetrix', JSON.stringify(response));
+//         });
+//       }
+//       map.setCenter(results[0].geometry.location);
+//       new google.maps.Marker({
+//         map,
+//         position: results[0].geometry.location,
+//       });
+//     })
+//     .catch((e) =>
+//       alert(`Geocode was not successful for the following reason: ${e}`)
+//     );
+// }
 
-window.initMap = () => {
-  const { _location, _destination } = JSON.parse(
-    localStorage.getItem('package')
-  );
-  const bounds = new google.maps.LatLngBounds();
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 6.5095, lng: 3.3711 },
-    zoom: 8,
-  });
-  // initialize services
-  const geocoder = new google.maps.Geocoder();
-  const service = new google.maps.DistanceMatrixService();
-  const addresses = [_destination, _location];
+// window.initMap = () => {
+//   const { _location, _destination } = JSON.parse(
+//     localStorage.getItem('package')
+//   );
+//   const bounds = new google.maps.LatLngBounds();
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     center: { lat: 6.5095, lng: 3.3711 },
+//     zoom: 8,
+//   });
+//   // initialize services
+//   const geocoder = new google.maps.Geocoder();
+//   const service = new google.maps.DistanceMatrixService();
+//   const addresses = [_destination, _location];
 
-  const add = [];
-  const parameters = { geocoder, map, add, service };
-  addresses.forEach((address) => {
-    geocodeAddress(parameters, address);
-  });
-  var input3 = document.getElementById('newDestination');
-  var autocomplete3 = new google.maps.places.Autocomplete(input3);
-};
+//   const add = [];
+//   const parameters = { geocoder, map, add, service };
+//   addresses.forEach((address) => {
+//     geocodeAddress(parameters, address);
+//   });
+//   var input3 = document.getElementById('newDestination');
+//   var autocomplete3 = new google.maps.places.Autocomplete(input3);
+// };
 
-function autoCompleteAddress() {
-  var input = document.getElementById('location');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  var input2 = document.getElementById('destination');
-  var autocomplete2 = new google.maps.places.Autocomplete(input2);
-  // autocomplete.addListener('place_changed', function () {
-  //   var place = autocomplete.getPlace();
-  //   document.getElementById('address').value = JSON.stringify(
-  //     place.address_components
-  //   );
-  // });
-}
+// function autoCompleteAddress() {
+//   var input = document.getElementById('location');
+//   var autocomplete = new google.maps.places.Autocomplete(input);
+//   var input2 = document.getElementById('destination');
+//   var autocomplete2 = new google.maps.places.Autocomplete(input2);
+//   // autocomplete.addListener('place_changed', function () {
+//   //   var place = autocomplete.getPlace();
+//   //   document.getElementById('address').value = JSON.stringify(
+//   //     place.address_components
+//   //   );
+//   // });
+// }
