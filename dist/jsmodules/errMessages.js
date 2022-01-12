be665da560d10b7a1d6b280107eaaf5fe6080546
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-12 19:14:42 +0100

    Implementing modules in the project

--- dist/jsmodules/errMessages.js
+++ dist/jsmodules/errMessages.js
@@ -9,4 +9,13 @@ const clearDisplayErr = () => {
   erro.innerHTML = '';
 };
 
-export { displayErr, clearDisplayErr };
+const setErrorFor = (inp, message) => {
+  inp.style.border = '1px solid red';
+  const inputFeild = inp.parentElement;
+  const small = inputFeild.querySelector('small');
+  small.style.visibility = 'visible';
+  small.style.color = 'red';
+  small.innerHTML = message;
+};
+
+export { displayErr, clearDisplayErr, setErrorFor };
--- dist/jsmodules/validateForm.js
+++ dist/jsmodules/validateForm.js
@@ -1,3 +1,5 @@
+import { setErrorFor } from './errMessages.js';
+
 const formValidation = (input) => {
   const pattern =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
