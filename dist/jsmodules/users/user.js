1e7ce66b02b27f1b4e0f410f506c84046bd8342c
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-12 23:55:58 +0100

    Implementing modules in the project

--- dist/jsmodules/users/user.js
+++ dist/jsmodules/users/user.js
@@ -42,14 +42,9 @@ window.logOut = () => {
 };
 window.displayUserPackages = async () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  const { _email, auth_token, users_id, _username } = user.user;
-  const userPackageUrl = `https://akera-logistics.herokuapp.com/api/v1/users/${_username}/${users_id}/${_email}/${auth_token}/packages`;
   if (!user.user.auth_token) {
     window.location.href = '/login';
   } else {
-    const userPackages = await fetchData(userPackageUrl);
-    localStorage.removeItem('packages');
-    localStorage.setItem('packages', JSON.stringify(userPackages));
     createUserPackage();
   }
 };
