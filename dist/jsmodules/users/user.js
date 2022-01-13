8399ff6aaeb68791ea4621d0c1adf6f801c40322
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-13 02:53:28 +0100

    Implementing modules in the project

--- dist/jsmodules/users/user.js
+++ dist/jsmodules/users/user.js
@@ -71,6 +71,7 @@ window.getPackage = (td) => {
   const { packages } = JSON.parse(localStorage.getItem('user'));
   console.log(packages);
   const packag = packages.filter((packageData) => {
+    console.log(packa);
     packageData.parcel_id === parseInt(parcelId);
   });
   console.log(packag);
