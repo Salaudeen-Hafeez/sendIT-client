6d4fdd92a2e4dd0f4cd110e2aa7d79cce3d317d6
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-13 17:24:45 +0100

    Implementing modules in the project

--- dist/jsmodules/packages/package.js
+++ dist/jsmodules/packages/package.js
@@ -108,9 +108,7 @@ window.updateDestination = async () => {
       window.location.reload();
     } else {
       const data = { _destination: data1.destination };
-      const { add1: add2 } = await geocodeAddress(geocoder, data1.destination);
-      console.log(add2);
-      const add = [_location, add2];
+      const add = [_location, data1.destination];
       const distMetrix = await getDistance(service, add);
       console.log(distMetrix);
       const updatedPackage = await putPackage(userUpdateUrl, data);
