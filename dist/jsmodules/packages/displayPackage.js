d9d24c0de18ce31d5c3afd02db4e7744d4f39598
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-14 18:04:37 +0100

    Implementing modules in the project

--- dist/jsmodules/packages/displayPackage.js
+++ dist/jsmodules/packages/displayPackage.js
@@ -4,9 +4,9 @@ const packageDisplay = (packages) => {
     packages.forEach((packag) => {
       packagesDiv += `<li>
         <div style="margin-bottom:8px; cursor: pointer" onclick="getPackage(this)" value= "${packag.parcel_id}" id="parcel-id">
-          <h4 style="color:red">${packag._name}</h4>
+          <h4 style="color:#095F06">${packag._name}</h4>
           <p>${packag._destination}</p>
-          <p style="color:green">${packag._status}</p>
+          <p style="color:#D70C0C">${packag._status}</p>
         </div>
       </li>
     `;
