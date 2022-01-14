180ce16ad04189db8fae3fe43dfd46bad488e72e
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-14 18:45:51 +0100

    Implementing modules in the project

--- dist/jsmodules/admins/admin.js
+++ dist/jsmodules/admins/admin.js
@@ -52,7 +52,7 @@ window.adminFetchUserPackage = (e) => {
 window.getPackage = (td) => {
   localStorage.removeItem('package');
   console.log(td);
-  const parcelId = td.value;
+  const parcelId = td.parentElement.value;
   console.log(parcelId);
   const packag1 = packages.filter((packag) => packag.parcel_id === parcelId);
   localStorage.setItem('package', JSON.stringify(packag1));
