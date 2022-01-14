7275592c8b53b128c43b604517cbef92506e4b98
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-14 12:39:36 +0100

    Implementing modules in the project

--- dist/jsmodules/users/postPackage.js
+++ dist/jsmodules/users/postPackage.js
@@ -1,6 +1,17 @@
 import { postData } from '../httpFetch/postData.js';
 import { postPackageUrl } from '../httpFetch/urls.js';
 import { formValidation } from '../validateForm.js';
+
+const input = document.getElementById('location');
+const autocomplete = new google.maps.places.Autocomplete(input);
+const input2 = document.getElementById('destination');
+const autocomplete2 = new google.maps.places.Autocomplete(input2);
+autocomplete.addListener('place_changed', function () {
+  var place = autocomplete.getPlace();
+  document.getElementById('address').value = JSON.stringify(
+    place.address_components
+  );
+});
 const user = JSON.parse(localStorage.getItem('user'));
 window.submitPackage = async () => {
   const input = document
