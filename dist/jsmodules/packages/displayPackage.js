7a9453ff592b1347513c38d98de5383aceea0789
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-14 18:12:06 +0100

    Implementing modules in the project

--- dist/jsmodules/packages/displayPackage.js
+++ dist/jsmodules/packages/displayPackage.js
@@ -1,12 +1,24 @@
 const packageDisplay = (packages) => {
   let packagesDiv = '';
+  let color;
   if (Array.isArray(packages) && packages.length !== 0) {
     packages.forEach((packag) => {
+      switch (packag._status) {
+        case 'In transit':
+          color = '#D70C0C';
+          break;
+        case 'Delivered':
+          color = 'blue';
+          break;
+        default:
+          color = 'red';
+          break;
+      }
       packagesDiv += `<li>
         <div style="margin-bottom:8px; cursor: pointer" onclick="getPackage(this)" value= "${packag.parcel_id}" id="parcel-id">
           <h4 style="color:#095F06">${packag._name}</h4>
           <p>${packag._destination}</p>
-          <p style="color:#D70C0C">${packag._status}</p>
+          <p style="color:${color}">${packag._status}</p>
         </div>
       </li>
     `;
