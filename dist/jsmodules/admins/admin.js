bb59ee9192af3d4e0551a5dc7aec860c830896b8
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-14 18:28:07 +0100

    Implementing modules in the project

--- dist/jsmodules/admins/admin.js
+++ dist/jsmodules/admins/admin.js
@@ -25,13 +25,15 @@ window.fetchUsers = () => {
   console.log(users);
   users.forEach((user) => {
     userul += `<li>
+        <div>
         <div class="userDetails">
           <h2>${user._name}</h2>
           <p>${user._username}</p>
           <p>${user._email}</p>
           <button onclick="adminFetchUserPackage(this)" value= "${user._username}" id="${user.users_id}">packages</button>
           <button onclick="adminDeleteUser(this)" value= "${user._username}" id="${user.users_id}">delete user</button>
-          <div class="userCont" id="userCont${user.users_id}"></div>
+        </div>
+        <div class="userCont" id="userCont${user.users_id}"></div>
         </div>
       </li>`;
   });
