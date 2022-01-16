436d57d87b89a2ddbad382de6a399adc9555964a
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-16 13:02:32 +0100

    Implementing modules in the project

--- dist/jsmodules/users/postPackage.js
+++ dist/jsmodules/users/postPackage.js
@@ -29,7 +29,7 @@ window.submitPackage = async () => {
     console.log(postedData);
     localStorage.removeItem('package');
     localStorage.setItem('package', JSON.stringify(postedData));
-    window.location.href = '/package';
+    //window.location.href = '/package';
   }
 };
 window.clearErr = (e) => {
