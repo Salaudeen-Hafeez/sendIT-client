21acd383e0b2e5aecd97b694c7016ccd59dc0851
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-18 09:15:30 +0100

    Implementing modules in the project

--- dist/jsmodules/packages/displayPackage.js
+++ dist/jsmodules/packages/displayPackage.js
@@ -1,6 +1,13 @@
 const packageDisplay = (packages, labels) => {
+  const admin = JSON.parse(localStorage.getItem('admin'));
   let packagesDiv = '';
+  let visible;
   let color;
+  if (!admin) {
+    visible = 'hidden';
+  } else {
+    visible = 'visible';
+  }
   if (Array.isArray(packages) && packages.length !== 0) {
     packages.forEach((packag) => {
       switch (packag._status) {
@@ -22,8 +29,8 @@ const packageDisplay = (packages, labels) => {
           <p style="font-weight:800;color:${color}">${packag._status}</p>
         </div>
         <div class="deleteDiv">
-        <button class="delete" onclick="adminDeletePackage(this)" value= "${packag._username}" id="${packag.parcel_id}">delete</button>
-        <button class="delete1" onclick="updatePackage(this)" value= "${packag._username}" id="${packag.parcel_id}">${labels}</button>
+        <button class="delete" style="visibility:${visible}" onclick="adminDeletePackage(this)" value= "${packag._username}" id="${packag.parcel_id}">delete</button>
+        <button class="delete" onclick="updatePackage(this)" value= "${packag._username}" id="${packag.parcel_id}">${labels}</button>
         </div>
       </li>
     `;
