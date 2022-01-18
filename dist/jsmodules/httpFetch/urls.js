bf8de7dc91d15a09a8537bc3bca61fccbe11cfee
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-18 14:32:53 +0100

    Implementing modules in the project

--- dist/jsmodules/httpFetch/urls.js
+++ dist/jsmodules/httpFetch/urls.js
@@ -1,24 +1,23 @@
-const user = JSON.parse(localStorage.getItem('user'));
-const admin = JSON.parse(localStorage.getItem('admin'));
 const packag = JSON.parse(localStorage.getItem('package'));
-let email, userid, token, username, id;
+const { _username, _email, users_id, auth_token } = JSON.parse(
+  localStorage.getItem('user')
+);
 if (packag !== null) {
   id = parseInt(packag.parcel_id);
 }
-if (user !== null) {
-  email = user._email;
-  username = user._username;
-  userid = user.users_id;
-  token = user.auth_token;
-} else if (admin !== null) {
-  email = admin._email;
-  userid = admin.users_id;
-  token = admin.admin_token;
-}
+
 const loginUrl = 'https://akera-logistics.herokuapp.com/api/v1/users/login';
 const postAdmUrl = 'https://akera-logistics.herokuapp.com/api/v1/users/admins';
 const postUsrUrl = 'https://akera-logistics.herokuapp.com/api/v1/users';
 const adminLoginUrl =
   'https://akera-logistics.herokuapp.com/api/v1/users/admins/login';
-const postPackageUrl = `https://akera-logistics.herokuapp.com/api/v1/users/${username}/${email}/${token}/packages`;
-export { loginUrl, postAdmUrl, postUsrUrl, postPackageUrl, adminLoginUrl };
+const userUpdateUrl = `https://akera-logistics.herokuapp.com/api/v1/users/${_email}/${users_id}/${auth_token}/packages/${id}`;
+const postPackageUrl = `https://akera-logistics.herokuapp.com/api/v1/users/${_username}/${_email}/${auth_token}/packages`;
+export {
+  loginUrl,
+  postAdmUrl,
+  postUsrUrl,
+  postPackageUrl,
+  adminLoginUrl,
+  userUpdateUrl,
+};
