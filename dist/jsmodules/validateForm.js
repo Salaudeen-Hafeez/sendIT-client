063723b4a014a49a62b397f4e415bea7422f155f
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-19 15:40:02 +0100

    Implementing modules in the project

--- dist/jsmodules/validateForm.js
+++ dist/jsmodules/validateForm.js
@@ -1,4 +1,11 @@
-import { setErrorFor } from './errMessages.js';
+const setErrorFor = (inp, message) => {
+  inp.style.border = '1px solid red';
+  const inputFeild = inp.parentElement;
+  const small = inputFeild.querySelector('small');
+  small.style.visibility = 'visible';
+  small.style.color = 'red';
+  small.innerHTML = message;
+};
 
 const formValidation = (input) => {
   const pattern =
