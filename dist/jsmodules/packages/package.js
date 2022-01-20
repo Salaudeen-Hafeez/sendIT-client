bf10821bded790db4295ee3931ec8e8254254d52
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-20 08:20:35 +0100

    Implementing modules in the project

--- dist/jsmodules/packages/package.js
+++ dist/jsmodules/packages/package.js
@@ -83,7 +83,7 @@ window.loadPackage = async () => {
 window.okay = () => {
   const location = document.getElementById('location');
   const status = document.getElementById('status');
-  console.log(location.innerHTML, status.innerHTML);
+  console.log(status.options[status.selectedIndex].value, status.innerHTML);
   // if (!user) {
   //   window.location.href = '/admin';
   // } else {
