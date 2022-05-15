263fda42a943b770c0973bb572e64c2f00abcbd1
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-05-15 23:14:10 +0100

    Working on the API end point

--- dist/jsmodules/admins/admin.js
+++ dist/jsmodules/admins/admin.js
@@ -1,8 +1,6 @@
 import { putPackage } from '../httpFetch/putData.js';
 import { packageDisplay } from '../packages/displayPackage.js';
 
-localStorage.clear();
-
 const geocodeAddress = async (geocoder, address) => {
   let geocodeResult = await geocoder
     .geocode({ address })
@@ -157,7 +155,6 @@ window.adminDeletePackage = (e) => {
       )
         .then((resp) => resp.json())
         .then((data) => {
-          console.log(data);
           localStorage.removeItem('packages');
           localStorage.setItem('packages', JSON.stringify(data.packages));
           if (button !== null) {
