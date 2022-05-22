43e4c5479f577cf57f28e8b2f7ab23690cc8cb83
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-05-22 13:03:05 +0100

    Working on the status update button

--- dist/jsmodules/packages/package.js
+++ dist/jsmodules/packages/package.js
@@ -155,3 +155,8 @@ window.canceleOrder = async () => {
     }
   }
 };
+window.clearErr = (e) => {
+  e.style.border = '1px solid lightgreen';
+  const small = e.parentElement.querySelector('small');
+  small.style.visibility = 'hidden';
+};
