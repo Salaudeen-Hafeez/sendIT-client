05bf48b3b22a2b09298b5abc1892b7680571f8ef
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-05 11:29:21 -0400

    working on user activieties

--- dist/jsmodules/httpFetch/urls.js
+++ dist/jsmodules/httpFetch/urls.js
@@ -2,4 +2,4 @@ const loginUrl = 'https://akera-backend.herokuapp.com/api/v1/login';
 const postUsrUrl = 'https://akera-backend.herokuapp.com/api/v1/signup';
 const fetchParcelUrl = `https://akera-backend.herokuapp.com/api/v1/parcels/user`;
 const postParcelUrl = `https://akera-backend.herokuapp.com/api/v1/parcels`;
-export { loginUrl, postUsrUrl, updateParcelUrl, fetchParcelUrl, postParcelUrl };
+export { loginUrl, postUsrUrl, fetchParcelUrl, postParcelUrl };
