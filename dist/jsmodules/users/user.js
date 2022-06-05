62ef04e67f31e527123c7e122fb56ceabaea6565
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-05 10:43:07 -0400

    working on user activieties

--- dist/jsmodules/users/user.js
+++ dist/jsmodules/users/user.js
@@ -1,7 +1,7 @@
 import { fetchData } from '../httpFetch/fetchData.js';
 import { fetchParcelUrl } from '../httpFetch/urls.js';
 import { packageDisplay } from '../packages/displayPackage.js';
-
+localStorage.clear()
 const geocodeAddress = async (geocoder, address) => {
   let geocodeResult = await geocoder
     .geocode({ address })
@@ -79,6 +79,7 @@ window.logOut = async () => {
 window.displayUserPackages = async () => {
   const packages = JSON.parse(localStorage.getItem('packages'));
   const packagesDiv = document.getElementById('packagesUl');
+  console.log(packages)
   if (user !== null && !user.auth_token) {
     window.location.href = '/login';
   } else {
